f80572f07215fdcb4c2e614064177388800908e0
Helen <zybina.e@mail.ru>
2023-01-14 16:42:18 +0300

    ДЗ c# задача 34

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 34. Задайте массив заполненный случайными трехзначными положительными числами.Напишите программу, которая покажет количество четных
+// чисел в массиве.
+int[] array = new int[9];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(150, 160);
+    Console.WriteLine($"{array[i]}");
+    if (array[i] % 2 == 0)
+    {
+       count=count+1;
+    }
+}
+
+Console.Write(count);
